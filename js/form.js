1faeef2900910dae6b65be2d65aeeb4d06279428
Roman <psionik@bk.ru>
2023-04-09 19:50:28 +0700

    disable esc key down when text in focus

--- js/form.js
+++ js/form.js
@@ -1,16 +1,26 @@
+
 const uploadImageForm = document.querySelector('.img-upload__form');
 const uploadOverlay = uploadImageForm.querySelector('.img-upload__overlay');
 const uploadImageBtn = uploadImageForm.querySelector('.img-upload__input');
 const closeUploadImageWindowbtn = uploadImageForm.querySelector('.img-upload__cancel');
+const textArea = document.querySelector('.text__description');
+const hashTags = document.querySelector('.text__hashtags');
 const body = document.body;
+const uploadContainer = uploadImageForm.querySelector('.img-upload__wrapper');
 
 
 uploadImageBtn.addEventListener('change', showUploadImageWindow);
 
 function showUploadImageWindow() {
   uploadOverlay.classList.remove('hidden');
-  document.addEventListener('keydown', isEscDown);
   closeUploadImageWindowbtn.addEventListener('click', hideUploadImageWindow);
+  uploadContainer.addEventListener('click', (e) => {
+    if(e.target == textArea || e.target == hashTags) {
+      document.removeEventListener('keydown', isEscDown);
+    }else {
+      document.addEventListener('keydown', isEscDown);
+    }
+  });
   isShowModal();
 }
 
@@ -18,6 +28,7 @@ function hideUploadImageWindow() {
   uploadImageBtn.value = '';
   uploadOverlay.classList.add('hidden');
   document.removeEventListener('keydown',isEscDown);
+  uploadImageForm.reset();
   isShowModal();
 }
 
@@ -29,8 +40,36 @@ function isEscDown(evt) {
 }
 
 function isShowModal() {
-  uploadOverlay.classList.contains('hidden') ? body.classList.remove('modal-open') : body.classList.add('modal-open');
+  if(uploadOverlay.classList.contains('hidden')){
+    body.classList.remove('modal-open');
+  }else {
+    body.classList.add('modal-open');
+  }
 }
 
 //Validate form with Pristine.js
 
+const pristine = new Pristine(uploadImageForm, {
+  classTo: 'text__label',
+  errorTextParent: 'text__label',
+  errorTextClass: 'text__label--error',
+});
+
+uploadImageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const isValid = pristine.validate();
+
+  if(isValid) {
+    uploadImageForm.submit(deleteFormRedirect());
+  }else {
+    e.preventDefault();
+  }
+  console.log(isValid);
+});
+
+
+function deleteFormRedirect() {
+  const data = new FormData(uploadImageForm);
+  fetch(uploadImageForm.action, {method:uploadImageForm.method, body: data}).then(response => console.log(response));
+  uploadImageForm.reset();
+}
